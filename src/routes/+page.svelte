6ee8d9fb793d322d8309dfcfaa8a4a7208fe6584
Travis <twag117@gmail.com>
2026-05-07 18:34:45 -0500

    Working MVP: random word with image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,21 @@
 <script>
-  import Words from '$lib/data/words.json'
+  import words from '$lib/data/words.json'
+  import { onMount } from 'svelte'
 
-  let words = Words
-  let randomNumber = Math.floor(Math.random() * 1000) + 1
-  let firstWord = words[randomNumber]
-  console.log(firstWord)
+  let selectedWord = $state(words[0])
+
+  function pickRandom() {
+    selectedWord = words[Math.floor(Math.random() * 1000)]
+  }
+
+  onMount(pickRandom)
 
 </script>
 
 <h1>Welcome to Flash Espanol</h1>
-<p>{firstWord.es}</p>
-
+<button onclick={pickRandom}>Next</button>
+<h3>{selectedWord.es}</h3>
+<img src="https://image.pollinations.ai/prompt/{selectedWord.en}" alt="{selectedWord.es}">
 
 <style>
   * {
