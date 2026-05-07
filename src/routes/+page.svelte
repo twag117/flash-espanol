7c887143cc93cb9338ddd6b80b385a3b610723d1
Travis <twag117@gmail.com>
2026-05-07 18:06:51 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Words from '$lib/data/words.json'
+
+  let words = Words
+  let randomNumber = Math.floor(Math.random() * 1000) + 1
+  let firstWord = words[randomNumber]
+  console.log(firstWord)
+
+</script>
+
+<h1>Welcome to Flash Espanol</h1>
+<p>{firstWord.es}</p>
+
+
+<style>
+  * {
+    padding: 1rem;
+  }
+</style>
