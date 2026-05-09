1548f5024b2bc69754275c056768526c944fbcc6
Travis <twag117@gmail.com>
2026-05-09 16:03:38 -0500

    some ui updates and testing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,21 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<header class="header">
+	<h6>Flash Español</h6>
+</header>
+
+
+<main>
+	{@render children()}
+</main>
+
+<footer>
+	© 2026 TechForge 365. All Rights Reserved.
+</footer>
+
+<style>
+	header, main, footer {
+		padding: var(--vs-base);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,20 +5,38 @@
   let selectedWord = $state(words[0])
 
   function pickRandom() {
-    selectedWord = words[Math.floor(Math.random() * 1000)]
+    selectedWord = words[Math.floor(Math.random() * 450)]
   }
 
   onMount(pickRandom)
 
 </script>
 
-<h1>Welcome to Flash Espanol</h1>
-<button onclick={pickRandom}>Next</button>
-<h3>{selectedWord.es}</h3>
-<img src="https://image.pollinations.ai/prompt/{selectedWord.en}" alt="{selectedWord.es}">
+<div class="stack box" style="max-inline-size: 500px">
+  <article class="card">
+    <figure>
+      <img
+        src="https://image.pollinations.ai/prompt/{selectedWord.en}"
+        width=622 height=350
+        alt="Image_Not_Found"
+      />
+    </figure>
+    <header>
+      <h4>{selectedWord.es}</h4>
+      <span class="tag muted">{selectedWord.type}</span>
+    </header>
+    <div class="card-body stack">
+      <p>{selectedWord.pronunciation}</p>
+    </div>
+    <footer class="cluster">
+      <button onclick={pickRandom}>Next</button>
+    </footer>
+  </article>
+
+</div>
+
 
 <style>
-  * {
-    padding: 1rem;
-  }
+
+
 </style>
