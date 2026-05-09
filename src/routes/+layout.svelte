4a0d05b467f66db2d99f0d302cc8649cb1f96d02
Travis <twag117@gmail.com>
2026-05-09 16:17:23 -0500

    changed favicon again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,9 @@
 
 <svelte:head>
 	<!-- <link rel="icon" href={favicon} /> -->
-	<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%25%22 y=%2250%25%22 dominant-baseline=%22middle%22 text-anchor=%22middle%22 font-size=%2290%22 fill=%22white%22>Ñ</text></svg>">
+	<!-- <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%25%22 y=%2250%25%22 dominant-baseline=%22middle%22 text-anchor=%22middle%22 font-size=%2290%22 fill=%22white%22>Ñ</text></svg>"> -->
+	<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22 fill=%22white%22>Ñ</text></svg>">
+
 </svelte:head>
 
 <header class="header">
